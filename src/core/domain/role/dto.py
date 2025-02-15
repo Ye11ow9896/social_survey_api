8c66b831011a37092b222131f85e6b1abd323530
Kondratenko <aleksei.kondratenko@strana.com>
2025-02-15 09:22:35 +0300

    mypy integration

--- src/core/domain/role/dto.py
+++ src/core/domain/role/dto.py
@@ -1,10 +1,1 @@
-from uuid import UUID
-
-from database.models.role import RoleCodeEnum
-from src.core.dto import BaseDTO
-
-
-class RoleDTO(BaseDTO):
-    id: UUID
-    name: str
-    code: RoleCodeEnum
+...
