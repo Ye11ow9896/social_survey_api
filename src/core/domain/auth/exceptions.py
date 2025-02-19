dbe89be5a0e27349470686715741bab0036c5551
Kondratenko <aleksei.kondratenko@strana.com>
2025-02-19 10:51:41 +0300

    add access middleware

--- src/core/domain/auth/exceptions.py
+++ src/core/domain/auth/exceptions.py
@@ -7,6 +7,13 @@ class TokenEncodeError(Exception): ...
 class BadPasswordError(Exception): ...
 
 
+class TokenNotFoundInHeaders(Exception): ...
+
 class UnauthorizedError(Exception):
     def __init__(self, message: str | None) -> None:
         self.message = message
+
+class TokenExpiredError(Exception):
+    def __init__(self) -> None:
+        self.message = "Срок токена истек"
+
