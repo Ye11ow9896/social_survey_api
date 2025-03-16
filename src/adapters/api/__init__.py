664934141dd52435a8c4fd3c1996c30f93d3f7c9
KoFeVarKaA <155472727+KoFeVarKaA@users.noreply.github.com>
2025-03-16 20:55:47 +0300

    Error:sqlalchemy.exc.NoReferencedTableError

--- src/adapters/api/__init__.py
+++ src/adapters/api/__init__.py
@@ -1,10 +1,12 @@
-from src.adapters.api.auth.controller import AuthController
 from litestar.types import ControllerRouterHandler
+from src.adapters.api.auth.controller import AuthController
 from src.adapters.api.common.controller import CommonController
+from src.adapters.api.survey.controller import SurveyController
 from src.adapters.api.telegram_user.controller import TelegramUserController
 
 route_handlers: list[ControllerRouterHandler] = [
     CommonController,
     AuthController,
     TelegramUserController,
+    SurveyController,
 ]
