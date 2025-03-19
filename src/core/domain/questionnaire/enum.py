f6e0a50aa43391a25d19b1ff747229a966ac86b4
Kondratenko <aleksei.kondratenko@strana.com>
2025-03-19 21:23:53 +0300

    create table and migration

--- src/core/domain/questionnaire/enum.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-
-class QuestionType(enum.StrEnum):
-    WRITTEN = "written"
-    MULTIPLE_CHOICE = "multiple_choice"
-    ONE_CHOICE = "one_choice"
--- src/database/models/questionnaire.py
+++ src/database/models/questionnaire.py
@@ -1,12 +1,29 @@
 from typing import Any
+from uuid import UUID
+
 from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy.orm import Mapped, mapped_column
-from .base import Base, create_comment
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy import String, ARRAY
+
+from src.database.models.base import Base, create_comment
+from src.database.enums import QuestionType
 
 
 class Questionnaire(Base):
     __tablename__ = "questionnaire"
-    __table_args__ = create_comment("Таблица для сохранения структуры анкеты")
+    __table_args__ = create_comment("Таблица для хранения анкеты")
 
+    question_id: Mapped[UUID | None] = mapped_column(ForeignKey("questionnaire_question.id"), )
     name: Mapped[str | None]
-    data: Mapped[dict[str, Any]] = mapped_column(JSONB)
+
+    questionnaire_questions: Mapped[list["QuestionnaireQuestion"] | None] = relationship()
+
+class QuestionnaireQuestion(Base):
+    __tablename__ = "questionnaire_question"
+    __table_args__ = create_comment("Таблица для хранения вопросов для анкеты")
+
+    question_text: Mapped[str] = mapped_column(comment="Текст вопроса анкеты")
+    number: Mapped[int] = mapped_column(comment="Порядковый номер вопроса анкеты")
+    choice_text: Mapped[list[str] | None] = mapped_column(ARRAY(String), default=None, comment="Список вопросов для множественного выбора. Зависит от типа")
+    question_type: Mapped[QuestionType] = mapped_column(comment="Тип вопроса")
