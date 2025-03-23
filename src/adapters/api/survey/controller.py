53e8a8286561c641468a8eb99cd3b3d8378282bf
KoFeVarKaA <155472727+KoFeVarKaA@users.noreply.github.com>
2025-03-23 13:56:39 +0300

    create get survey/all endpoint

--- src/adapters/api/survey/controller.py
+++ src/adapters/api/survey/controller.py
@@ -1,20 +1,49 @@
 from http import HTTPStatus
-from typing import Any
+from inspect import Parameter
+from typing import Annotated, Any
+from result import Err
 
-from src.adapters.api.schema import APIDetailSchema
-from src.adapters.api.survey.schema import SurveyCreateSchema
-from src.core.domain.survey.service import SurveyService
-from src.core.domain.auth.middleware import CheckAccessTokenMiddleware
-from litestar import Response, post
+from litestar import Response, get, post
 from litestar.controller import Controller
+from litestar.params import Parameter
 from aioinject import Injected
 from aioinject.ext.litestar import inject
+
+from src.lib.paginator import PaginationDTO, PaginationResultDTO
+from src.adapters.api.schema import APIDetailSchema
+from src.adapters.api.survey.schema import SurveyCreateSchema
+from src.adapters.api.exceptions import ObjectNotFoundHTTPError
+from src.core.domain.survey.dto import SurveyDTO
+from src.core.domain.survey.service import SurveyService
+from src.core.domain.auth.middleware import CheckAccessTokenMiddleware
 
 
 class SurveyController(Controller):
     path = "/survey"
     tags = ("Survey endpoints",)
     middleware = [CheckAccessTokenMiddleware]
+
+    @get("/all", status_code=200)
+    @inject
+    async def get_all(
+        self,
+        name: str | None,
+        service: Injected[SurveyService],
+        page_size: Annotated[
+            int, Parameter(ge=1, le=1_000, query="pageSize")
+        ] = 100,
+        page: Annotated[int, Parameter(ge=1)] = 1,
+    ) -> PaginationResultDTO[SurveyDTO]:
+        pagination_dto = PaginationDTO(
+            page_size=page_size,
+            page=page,
+        )
+        result = await service.get_all(
+            pagination_dto, name
+        )
+        if isinstance(result, Err):
+            raise ObjectNotFoundHTTPError(message=result.err_value.message)
+        return result.ok()
 
     @post("/create", status_code=200)
     @inject
--- src/core/domain/survey/service.py
+++ src/core/domain/survey/service.py
@@ -1,3 +1,9 @@
+from result import Err, Ok, Result
+from sqla_filter import or_unset
+
+from src.lib.paginator import PagePaginator, PaginationResultDTO, PaginationDTO
+from src.core.domain.survey.dto import SurveyDTO, SurveyFilterDTO
+from src.core.exceptions import ObjectNotFoundError
 from src.core.domain.survey.repository import SurveyRepository
 from src.adapters.api.survey.dto import SurveyCreateDTO
 from src.database.models import Survey
@@ -7,9 +13,27 @@ class SurveyService:
     def __init__(
         self,
         user_repository: SurveyRepository,
+        paginator: PagePaginator,
     ) -> None:
-        self._repository = user_repository
+        self._survey_repository = user_repository
+        self._paginator = paginator
 
     async def create(self, dto: SurveyCreateDTO) -> Survey:
-        new_survey = await self._repository.create(dto=dto)
+        new_survey = await self._survey_repository.create(dto=dto)
         return new_survey
+
+    async def get_all(
+        self,
+        pagination: PaginationDTO,
+        name: str | None,
+    ) -> Result[PaginationResultDTO[SurveyDTO], ObjectNotFoundError]:
+        filter_dto = SurveyFilterDTO(
+            name=or_unset(name),
+        )
+        stmt = await self._survey_repository.get_all_stmt(filter_=filter_dto)
+        result = await self._paginator.paginate(
+            stmt, dto_model=SurveyDTO, pagination=pagination
+        )
+        if not result.items:
+            return Err(ObjectNotFoundError(obj=Survey.__name__))
+        return Ok(result)
