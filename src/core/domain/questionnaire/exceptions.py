359b45b63d605045ea24553fd851822d064f709f
KoFeVarKaA <155472727+KoFeVarKaA@users.noreply.github.com>
2025-03-26 20:41:03 +0300

    create question endpoint

--- src/core/domain/questionnaire/exceptions.py
+++ src/core/domain/questionnaire/exceptions.py
@@ -1,3 +1,17 @@
+class QuestionCreateUpdateQuestionError(Exception):
+    def __init__(self, question_name: str | None = None) -> None:
+        self.message = "Ошибка создания/обновления анкеты."
+        if question_name:
+            self.message += f" В вопросе `{question_name}` должно быть заполнено одно поле вопроса"
+
+
+class QuestionCreateUpdateMismatchError(Exception):
+    def __init__(self, question_name: str | None = None) -> None:
+        self.message = "Ошибка создания/обновления анкеты."
+        if question_name:
+            self.message += f" В вопросе `{question_name}` заполненное поле не соответствует его типу"
+
+
 class QuestionnaireCreateUpdateQuestionError(Exception):
     def __init__(self, question_name: str | None = None) -> None:
         self.message = "Ошибка создания/обновления анкеты."
